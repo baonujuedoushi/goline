9609aabda94d2312582c3ac57c5a26bf9036da84
hkq <baonujuedoushi@163.com>
2026-03-28 11:07:07 +0800

    optimize:more redis sample

--- golang/base/redis.go
+++ golang/base/redis.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -14,6 +15,7 @@ func redisSample() {
 		DB:       0,                // 使用的数据库
 		PoolSize: 10,               // 连接池大小
 	})
+	defer rdb.Close()
 
 	ctx := context.Background()
 	pong, err := rdb.Ping(ctx).Result()
@@ -21,4 +23,29 @@ func redisSample() {
 		panic(err)
 	}
 	fmt.Println("连接成功:", pong)
+
+	userID := "1001"
+	userKey := fmt.Sprintf("user:%s", userID)
+
+	rdb.HSet(ctx, userKey, map[string]any{
+		"name": "test user",
+		"age":  25,
+	})
+	rdb.Expire(ctx, userKey, 1*time.Hour)
+
+	user, _ := rdb.HGetAll(ctx, userKey).Result()
+	fmt.Printf("user info: %+v\n", user)
+
+	visitKey := fmt.Sprintf("visits:%s", userID)
+	count, _ := rdb.Incr(ctx, visitKey).Result()
+	fmt.Printf("visitis counts: %d\n", count)
+
+	pipe := rdb.Pipeline()
+	for i := 0; i < 10; i++ {
+		pipe.SAdd(ctx, "visited_users", fmt.Sprintf("user_%d", i))
+	}
+	pipe.Exec(ctx)
+
+	users, _ := rdb.SMembers(ctx, "visited_users").Result()
+	fmt.Printf("has visited: %v\n", users)
 }
